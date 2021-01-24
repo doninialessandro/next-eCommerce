354876351febe7e62280b03180ecb971f0e3a63b
Alessandro Donini <alessandro.donini.tech@gmail.com>
2021-01-24 21:11:36 +0100

    chore(footer): add version

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,10 +1,12 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import { Flex, Text, Link } from '@chakra-ui/react'
 
+import { version } from '../../../package.json'
+
 const Footer = () => (
   <Flex as="footer" py="2rem">
     <Text fontSize="xs">
-      Devlop with ❤️ from 🏠 by{' '}
+      v{version} - Develop with ❤️ from 🏠 by{' '}
       <Link href="https://github.com/doninialessandro" isExternal>
         Ale
       </Link>
